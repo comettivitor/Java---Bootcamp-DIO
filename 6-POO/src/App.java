570f384e378144719c4e37c5e285611e22af92d8
Vitor Cometti <comettivitor@gmail.com>
2022-10-28 14:21:57 -0300

    Commit

---
+++ 6-POO/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Carro carro = new Carro();
+
+        carro.setCor("Amarelo");
+        carro.setModelo("Volvo");
+        carro.setCapacidadeTanque(60);
+
+        System.out.println(carro.getCor());
+        System.out.println(carro.getModelo());
+        System.out.println(carro.getCapacidadeTanque());
+        System.out.println(carro.totalValorTanque(4.99));
+
+        Carro carro2 = new Carro("Cinza", "Toyota", 48);
+
+        System.out.println(carro2.getCor());
+        System.out.println(carro2.getModelo());
+        System.out.println(carro2.getCapacidadeTanque());
+        System.out.println(carro2.totalValorTanque(4.99));
+
+    }
+}
---
+++ 6-POO/src/Carro.java
@@ -0,0 +1,45 @@
+public class Carro {
+    private String cor;
+    private String modelo;
+    private int capacidadeTanque;
+
+    //Sobrecargas de construtores
+    Carro(){
+
+    }
+
+    Carro(String cor, String modelo, int capacidadeTanque){
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    public void setCapacidadeTanque(int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+
+    double totalValorTanque(double valorCombustível){
+        return capacidadeTanque * valorCombustível;
+    }
+}
